79f7b24c7f21dbe642c73040e9cbc3e5305be6b3
xerowe <ivanov.m93@gmail.com>
2017-09-21 16:48:47 +0300

    Finish all of the section

--- js/main.js
+++
@@ -1,6 +0,0 @@
-$(window).load(function(){
- $('.gallery-container').find('img').each(function(){
-  var imgClass = (this.width/this.height > 1) ? 'wide' : 'tall';
-  $(this).addClass(imgClass);
- })
-})
