a8b12d49d2970d81d3ab3e2df389e309945b7e46
xerowe <ivanov.m93@gmail.com>
2017-09-21 13:00:08 +0300

    Start Working on gallery section

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(window).load(function(){
+ $('.gallery-container').find('img').each(function(){
+  var imgClass = (this.width/this.height > 1) ? 'wide' : 'tall';
+  $(this).addClass(imgClass);
+ })
+})
